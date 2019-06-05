bedf652890015e2a5280a9d14788b5501f77ea48
Miller5388 <millerfernandes53@gmail.com>
2019-06-04 21:49:08 -0300

    triangulo

--- PRIMEIRU/PRIMEIRU/triangulo.cs
+++ PRIMEIRU/PRIMEIRU/triangulo.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace PRIMEIRU
 {
-	class triangulo
+	class triangulo:figura
 	{
 		private double altura;
 		private double baset;
 
-	}
+        public double Altura { set => altura = value; }
+        public double Baset { set => baset = value; }
+        
+
+        public override double area()
+        {
+            return altura * baset / 2.0;
+        }
+
+    }
 }
