a464b266c704df1c8e5cc6ab5246152c6936ba51
Diego Reis Silveira <diego_26_10_1997@hotmail.com>
2019-06-04 21:18:01 -0300

    a

---
+++ PRIMEIRU/PRIMEIRU/quadrado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIMEIRU
+{
+	class quadrado
+	{
+
+		private double lado;
+
+		public double Lado { set => lado = value; }
+
+		override public double area()
+		{
+
+			return lado * lado;
+		}
+	}
+}
