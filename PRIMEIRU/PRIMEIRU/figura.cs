185e7302874676c09f091b8bd45350d57467427b
Diego Reis Silveira <diego_26_10_1997@hotmail.com>
2019-06-04 21:12:14 -0300

    a

---
+++ PRIMEIRU/PRIMEIRU/figura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIMEIRU
+{
+	abstract class figura
+	{
+		private string cor;
+		private string nome;
+
+		public string Cor { get => cor; set => cor = value; }
+		public string Nome { get => nome; set => nome = value; }
+
+		public figura()
+		{ }
+
+		abstract public double area();
+	}
+}
